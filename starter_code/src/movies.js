d06734eee98c5a3782df96474559674205b9b173
raveBolognesa <rafael.arrieta.gar@gmail.com>
2019-03-26 15:27:08 +0100

    .

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,51 +2,117 @@
 // Turn duration of the movies from hours to minutes 
 
 function turnHoursToMinutes(movies) {
-
+    let dataReturn = movies.map(movie => {
+        let newDuration = movie.duration.split(" ");
+        if (newDuration.length === 2) {
+            newDuration = parseInt(newDuration[0]) * 60 + parseInt(newDuration[1]);
+        } else {
+            if (newDuration[0].includes("h")) {
+                newDuration = parseInt(newDuration[0])*60;
+            } else {
+                newDuration = parseInt(newDuration);
+            }
+        }
+        return { ...movies, duration: newDuration };
+    })
+    return dataReturn;
 }
+
 
 // Get the average of all rates with 2 decimals 
 
 function ratesAverage(movies) {
-    var countOfFilms = movies.lenght;
-    var result = 0;
-    var allRates = movies.reduce((acum,movie) => 
-    acum += +movie.rate , 0 );
-    result = allRates / countOfFilms;
-    return parseFloat(result.toFixed(2));
-
+    let avgToReturn = movies.reduce((acc, movie) => {
+        if (movie.rate == 0){
+            return acc
+        }
+        return acc += parseFloat(movie.rate)
+    }, 0)
+    avgToReturn /= movies.length;
+    avgToReturn = parseFloat(avgToReturn.toFixed(2));
+    return avgToReturn
 }
 
 
 // Get the average of Drama Movies
-
 function dramaMoviesRate(movies) {
+    let dataReturn= movies.filter((movie)=>{
+        return movie.genre.includes("Drama")
+    })
+    if (dataReturn.length === 0) {
+        return undefined
+    }
+    dataReturn = ratesAverage(dataReturn)
+    return dataReturn;
+    
 }
 
 
 // Order by time duration, in growing order
 
 function orderByDuration(movies) {
-    var orderMovies = movies.sort((a, b) a,b)
+    let dataReturn = movies.sort((a,b)=>{
+        if (a.duration > b.duration){
+            return 1
+        } if (a.duration < b.duration) {
+            return -1
+        } if (a.title > b.title){
+            return 1
+        } if (a.title < b.title){
+            return -1
+        }
+        return 0;
+    })
+    return dataReturn;
 }
 
 
 // How many movies did STEVEN SPIELBERG
 
-function howManyMovies(params) {
-    
+function howManyMovies(movies) {
+    if (movies.length === 0) {
+        return undefined
+    }
+    let dataReturn = movies.filter((movie)=>{
+        return movie.director == "Steven Spielberg"
+    })
+    dataReturn = dataReturn.filter((movie)=>{
+        return movie.genre.includes("Drama")
+    })
+    dataReturn = dataReturn.length
+    return `Steven Spielberg directed ${dataReturn} drama movies!`
 }
 
 
 // Order by title and print the first 20 titles
 
 function orderAlphabetically(movies) {
-    
+    let dataReturn = movies.map((movie)=>{
+        return movie.title
+    })
+    dataReturn.sort()
+    return dataReturn.splice(0,20)
 }
 
 
 // Best yearly rate average
-
-function bestYearAvg(params) {
-    
-}
+function bestYearAvg(movies){
+    if(movies.length === 0){return;}
+   
+    var bestYear = 0, bestRate = 0;
+     let moviesByYear = {};
+     movies.forEach(movie => {
+       if(!moviesByYear[movie.year]){
+         moviesByYear[movie.year] =  [];
+       }
+       moviesByYear[movie.year].push(movie);
+     });
+     for(let year in moviesByYear){
+       let rate = ratesAverage(moviesByYear[year]);
+       if(rate > bestRate){
+         bestRate = rate;
+         bestYear = year;
+       }
+     }
+     return (`The best year was ${bestYear} with an average rate of ${bestRate}`);
+   }
